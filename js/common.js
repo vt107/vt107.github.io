d5b43cbcbcf82a463b6a920052ea6962db4a0002
Van Tho <thovan@bayard.co.jp>
2023-02-27 14:51:50 +0700

    Update code align php + js + active menu

--- js/common.js
+++ js/common.js
@@ -1,9 +1,26 @@
-$(document).ready(function() {
-  $('body').prepend('<div id="nav" class="mb-3"></div>');
+let debug = window.location.href.includes('test=1');
 
-  ['nav'].forEach(element => {
-    $.get(element + '.html').done(function(data) {
-      $('#' + element).html(data);
-    })
-  });
+$(document).ready(function() {
+    // Append widgets
+    $('head').append('<link rel="stylesheet" href="/css/styles.css">');
+    $('body').prepend('<div id="nav" class="mb-3"></div>');
+
+    ['nav'].forEach(element => {
+        $.get(element + '.html').done(function(data) {
+            $('#' + element).html(data);
+
+            if (element === 'nav' && debug) {
+                // Find active menu
+                let currentUrl = window.location.pathname;
+
+                $('.navbar-nav a').each(function() {
+                    if (currentUrl === $(this).attr('href')) $(this).parent().addClass('active');
+
+                    $(this).attr('href', $(this).attr('href') + '?test=1')
+                });
+            }
+        })
+    });
 });
+
+
